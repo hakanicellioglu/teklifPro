6a6ce9b45ed12b4fc3f061078a3ceba1c00eaa4a
Hakan Berke İçellioğlu <hakanicellioglu>
2025-08-13 09:46:00 +0300

    remove-backups

--- yedek/pdf/templates/quotation.footer.php
+++
@@ -1,9 +0,0 @@
-<?php
-/** @var array $company */
-?>
-<table width="100%">
-  <tr>
-    <td style="width:50%;"><?= e($company['email']) ?> • <?= e($company['phone']) ?></td>
-    <td style="width:50%; text-align:right;">Sayfa {PAGENO}/{nbpg}</td>
-  </tr>
-</table>
