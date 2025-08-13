7efe4bd8a3536cb220168d1f67bdbf4874cac0f0
Hakan Berke İÇELLİOĞLU <95232280+hakanicellioglu@users.noreply.github.com>
2025-08-13 14:01:11 +0300

    Center table headers in customer and quotations pages

--- components/data_table.php
+++ components/data_table.php
@@ -1,10 +1,10 @@
 <?php
-function data_table_start(array $headers): void {
+function data_table_start(array $headers, string $headerClass = ''): void {
 ?>
 <div class="table-responsive">
 <table class="table table-hover align-middle">
   <thead class="table-light sticky-top">
-    <tr>
+    <tr<?= $headerClass ? ' class="' . htmlspecialchars($headerClass, ENT_QUOTES, 'UTF-8') . '"' : '' ?>>
       <?php foreach ($headers as $h): ?>
         <th scope="col"><?= htmlspecialchars($h, ENT_QUOTES, 'UTF-8'); ?></th>
       <?php endforeach; ?>
