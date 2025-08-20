0cfef4e2b8d735fbee24d5d4077674712180075d
Hakan Berke İÇELLİOĞLU <95232280+hakanicellioglu@users.noreply.github.com>
2025-08-20 14:22:55 +0300

    Log glass type from query

--- test.php
+++ test.php
@@ -47,6 +47,7 @@ if (!$system) {
 }
 
 $glassTypeRaw = (string)($system['glass_type'] ?? '');
+error_log('glass_type from query: ' . $glassTypeRaw);
 $glassTypeNorm = function_exists('mb_strtolower') ? mb_strtolower($glassTypeRaw, 'UTF-8') : strtolower($glassTypeRaw);
 $glassTypeNorm = str_replace(' ', '', $glassTypeNorm);
 $isInsulated   = in_array($glassTypeNorm, ['ısıcam', 'isicam'], true);
